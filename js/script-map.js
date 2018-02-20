f41fc61b40e06adfc85f1728936d17efb7f6b03d
Grione <35578611+Grione@users.noreply.github.com>
2018-02-20 13:24:36 +0300

    Add files via upload

---
+++ js/script-map.js
@@ -0,0 +1,78 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.964434, 30.509263],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '/img/icon/map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+
+        myPlacemarkWithContent = new ymaps.Placemark([59.968565, 30.350648], {
+            hintContent: 'Собственный значок метки с контентом',
+            balloonContent: 'А эта — новогодняя',
+            iconContent: '12'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: '/img/icon/map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+        myPlacemarkWithContent1 = new ymaps.Placemark([60.055197, 30.346528], {
+            hintContent: 'Собственный значок метки с контентом',
+            balloonContent: 'А эта — новогодняя',
+            iconContent: '12'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: '/img/icon/map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent)
+        .add(myPlacemarkWithContent1);
+
+    myMap.behaviors.disable('scrollZoom');    
+});
---
+++ js/script.js
@@ -0,0 +1,183 @@
+
+
+// menu-mobile
+
+var navMobile = document.querySelector('.mobile-nav');
+const button = document.querySelector('.mobile-menu');
+
+
+button.addEventListener('click', function (evt)
+{   evt.preventDefault();
+    navMobile.classList.add('mobile-nav_activ');
+}
+);
+
+var buttonClose = document.querySelector('.mobile-nav_close');
+
+buttonClose.addEventListener('click', function (evt)
+
+{   evt.preventDefault();
+    navMobile.classList.remove('mobile-nav_activ');
+}
+);
+
+var itemMobile = $('.mobile-nav_link');
+
+for (let i = 0; i < itemMobile.length; i++) {
+    itemMobile[i].addEventListener("click", function(e) {
+
+        e.preventDefault();
+
+        this.closest('.mobile-nav').classList.remove("mobile-nav_activ");
+    });
+}; 
+//team acco
+
+
+var teamItem = document.querySelectorAll('.item-team');
+const teamLink = document.querySelectorAll('.team-link');
+
+for (let i = 0; i < teamLink.length; i++) {
+    teamLink[i].addEventListener("click", function(e) {
+
+        e.preventDefault();
+
+        for (let j = 0; j < teamItem.length; j++) {
+            if (j !== i) {
+                teamItem[j].classList.remove("item-team_activ");
+            };
+        };
+
+        this.closest('.item-team').classList.toggle("item-team_activ");
+    });
+}; 
+
+// menu acco
+
+var menuItem = document.querySelectorAll('.menu-item');
+var menuLink = document.querySelectorAll('.menu-item_link');
+var menuTxt = document.querySelectorAll('.menu-link_txt');
+
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener("click", function(e) {
+
+        e.preventDefault();
+
+        for (let j = 0; j < menuItem.length; j++) {
+            if (j !== i) {
+                menuItem[j].classList.remove("menu-item_activ");
+            };
+        };
+
+            
+        this.closest('.menu-item').classList.toggle("menu-item_activ");
+        
+    });
+};
+
+//Модальное окно в отзывах
+
+function openPopup(e) {
+    e.preventDefault();
+    $('.reviews-modul').toggleClass('reviews-modul_active');
+};
+
+$('.reviews-item_button-link').on('click', openPopup);
+$('.window-close').on('click', openPopup);
+
+//One page scroll
+
+const sections = $('.section');
+const display = $('.maincontent');
+let inScroll = false;
+
+const setActiveMenuItem = itemEq => {
+    $('.radio-buttons-link').eq(itemEq).addClass('radio-buttons-link__activ')
+    .siblings().removeClass('radio-buttons-link__activ')
+  } 
+
+const performTransition = sectionEq => {
+    const position = `${sectionEq * -100}%`;
+
+    if (inScroll) return;
+    if (!inScroll) {
+
+    inScroll = true;
+
+    sections.eq(sectionEq).addClass('active')
+    .siblings().removeClass('active');
+
+    display.css ({
+        'transform' : `translate(0, ${position})` ,
+        '-webkit-transform' : `translate(0, ${position})`
+    })
+
+    setTimeout(() => {
+       inScroll = false; 
+       setActiveMenuItem(sectionEq);
+    }, 
+1300); // продолжительность анимации + 300ms (инерция)
+}
+}
+
+const scrollToSection = direction => {
+    const activeSection = sections.filter('.active');
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
+
+    if (direction == 'up' && prevSection.length) {
+        performTransition(prevSection.index())
+    }
+
+    if (direction == 'down' && nextSection.length) {
+        performTransition(nextSection.index())
+    }
+}
+
+
+$(document).on({
+    wheel: e => {
+    const deltaY = e.originalEvent.deltaY;
+    const direction = deltaY > 0 ? "down" : "up";
+    
+    scrollToSection(direction);
+    },
+    keydown: e => {
+        switch (e.keyCode) {
+            case 40 : scrollToSection('down');
+            case 38 : scrollToSection('up');
+        }
+    },
+
+    touchmove: e => e.preventDefault()
+
+});
+
+$('[data-scroll-to]').on('click' , e => {
+    e.preventDefault();
+
+    const target = $(e.currentTarget).attr('data-scroll-to');
+    
+    performTransition(target);
+})
+
+ $(document).swipe( {
+      //Generic swipe handler for all directions
+      swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+        
+        //плагин возвращает фактическое движение пальцев, нужно наоборот
+
+        const scrollDirection = direction === 'down' ? 'up' : 'down';
+        scrollToSection(scrollDirection);
+      }
+    });
+
+    /// slider
+    $(document).ready(function(){
+        $('.dark-center').slick({
+        
+        });
+      });
+
+
+  
